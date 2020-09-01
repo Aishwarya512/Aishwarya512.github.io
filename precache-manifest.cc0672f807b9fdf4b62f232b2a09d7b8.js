e94b333d2c21bfc514d14b93027a7e93857bcc17
Aishwarya Oza <Aishu@Aishwarya.local>
2020-08-31 18:18:16 -0700

    Updates

---
+++ precache-manifest.cc0672f807b9fdf4b62f232b2a09d7b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200f26220a39cd83ff88cd5245b40ac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "72680639fd21861762f0",
+    "url": "/static/css/main.bf23667e.chunk.css"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/js/2.0789156f.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.0789156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72680639fd21861762f0",
+    "url": "/static/js/main.a2077f21.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
